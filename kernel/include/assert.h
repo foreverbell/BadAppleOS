6521a98c9dd51f7c520b7bae8666be2088492fa4
foreverbell <dql.foreverbell@gmail.com>
2015-07-21 18:16:28 +0800

    alignment

--- kernel/include/assert.h
+++ kernel/include/assert.h
@@ -3,8 +3,8 @@
 
 #include <panic.h>
 
-#define STRINGIZE(x)    DO_STRINGIZE(x)
-#define DO_STRINGIZE(x)   #x
+#define STRINGIZE(x)     DO_STRINGIZE(x)
+#define DO_STRINGIZE(x)  #x
 
 #define assert(expr)    (void)( (!!(expr)) || (panic::panic("Assertion failed in file " __FILE__ ", line " STRINGIZE(__LINE__), 0), 0) )
 
