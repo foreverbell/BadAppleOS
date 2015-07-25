e96080374edfe4be193b890f76d3c61c6ae4fd2a
foreverbell <dql.foreverbell@gmail.com>
2015-07-25 14:28:51 +0800

    better prelude

--- kernel/ba/main.cc
+++ kernel/ba/main.cc
@@ -15,29 +15,37 @@ namespace {
 
 badapple::video v;
 
-void end_callback(uint64_t, int) {
-	systime_t *st = get_systime(NULL);
-	printf("\rCurrent system time = %d/%02d/%02d %02d:%02d:%02d.", st->year, st->month, st->day, st->hour, st->minute, st->second);	
-}
-
-void video_callback(uint64_t, int id) {
-	if (v.has_next()) {
-		v.next();
-		printf(" (%d%%) ", v.progress());
-	} else {
-		v.free();
-		timer::remove(id);
-		console::clear();
-		printf("Thank you for watching!\n");
-		printf("Repository can be found at https://github.com/foreverbell/BadAppleOS.\n");
-		timer::add(1, end_callback);
-	}
-}
-
 }
 
 void play(void) {
 	const int fps = 9;
 	
-	timer::add(TIMER_TICK_ONE_SECOND / fps, video_callback);
+	timer::add(TIMER_TICK_ONE_SECOND, [&](uint64_t count, int id) -> void {
+		if (count < 3) {
+			printf("\rBadApple, %d second(s) to go", 3 - count);
+			for (uint64_t i = 0; i <= count; ++i) {
+				printf(".");
+			}
+		} else {
+			timer::remove(id);
+			timer::add(TIMER_TICK_ONE_SECOND / fps, [&](uint64_t, int id) -> void {
+				if (v.has_next()) {
+					v.next();
+					printf(" (%d%%) ", v.progress());
+				} else {
+					timer::remove(id);
+					v.free();
+					console::clear();
+					printf("Thank you for watching!\n");
+					printf("Repository can be found at https://github.com/foreverbell/BadAppleOS.\n");
+					timer::add(1, [&](uint64_t, int) -> void {
+						systime_t *st = get_systime(NULL);
+						printf("\rCurrent system time = %d/%02d/%02d %02d:%02d:%02d.", 
+							st->year, st->month, st->day, 
+							st->hour, st->minute, st->second);	
+					});
+				}
+			});
+		}
+	});
 }
