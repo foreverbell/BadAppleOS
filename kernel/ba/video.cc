d486288a9dacfadf09d4f6238c3519b36225816d
foreverbell <dql.foreverbell@gmail.com>
2015-07-25 21:09:32 +0800

    artify ASCII art.

--- kernel/ba/video.cc
+++ kernel/ba/video.cc
@@ -4,7 +4,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <new>
+#include <vector>
+#include <pair>
 #include "video.h"
+
+using std::vector;
+using std::pair;
+using std::make_pair;
 
 namespace badapple {
 
@@ -39,15 +45,140 @@ video::video(void) {
 #ifdef VIDEO_SQUARE_TEXT
 			attrib = (raw & 1 ? bkcolor1 : bkcolor2) << 8;
 #else
-			attrib = (acolor << 8) | (raw & 1 ? ' ' : '*');
+			attrib = (acolor << 8) | (raw & 1 ? ' ' : '#');
 #endif
 			raw = raw >> 1;
 			*pool_ptr++ = attrib;
 		}
 		++raw_ptr;
 	}
-	
 	printf("[video] Data loaded, tailing pointer = 0x%x.\n", (int) pool_ptr);
+
+#ifndef VIDEO_SQUARE_TEXT
+	puts("[video] Artifying data, this may take a few second.");
+	artify();
+	puts("[video] Data artified.");
+#endif
+}
+
+char video::at(int frame, int x, int y) const {
+	const uint16_t *p = pool;
+	p += frame * VIDEO_SIZE;
+	p += y * VIDEO_MAX_COLUMN;
+	p += x;
+	return *(const char *) p;
+}
+
+void video::set(int frame, int x, int y, char ch) {
+	const uint16_t *p = pool;
+	p += frame * VIDEO_SIZE;
+	p += y * VIDEO_MAX_COLUMN;
+	p += x;
+	*(char *) p = ch;
+}
+
+std::pair<int, char> video::find(int x) {
+	if (dsu[x].first != x) {
+		dsu[x] = find(dsu[x].first);
+	}
+	return dsu[x];
+}
+
+void video::join(int x, int y) {
+	x = find(x).first;
+	y = find(y).first;
+	if (dsu[x].second == '?') {
+		dsu[x] = dsu[y];
+	} else {
+		dsu[y] = dsu[x];
+	}
+}
+
+void video::artify(void) {
+	const int dxy[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	
+	const char dot_chars[4] = {'\'', '`', ',', '.'};
+	const char line_chars[4] = {'\\', '/', char(28), '^'};
+	const char dense_chars[7] = {'M', 'H', 'W', '#', '*', '&', '@'};
+	
+	dsu.resize(VIDEO_SIZE);
+	
+	auto label = [&](int x, int y) -> int {
+		return y * VIDEO_MAX_COLUMN + x;
+	};
+	auto within = [&](int x, int y) -> bool {
+		return x >= 0 && x < VIDEO_MAX_COLUMN && y >= 0 && y < VIDEO_MAX_ROW;
+	};
+	
+	/* randomly change connected block to one dense char. */
+	for (int f = 0; f < count; ++f) {
+		for (int x = 0; x < VIDEO_MAX_COLUMN; ++x) {
+			for (int y = 0; y < VIDEO_MAX_ROW; ++y) {
+				char ch = at(f, x, y);
+				if (ch != ' ') {
+					if (f > 0 && at(f - 1, x, y) != ' ') {
+						/* 1% probably to get a mutation. */
+						ch = rand() % 100 == 0 ? dense_chars[rand() % 7] : at(f - 1, x, y);
+					} else {
+						ch = '?'; // to be determinated.
+					}
+				}
+				dsu[label(x, y)] = make_pair(label(x, y), ch);
+			}
+		}
+		for (int x = 0; x < VIDEO_MAX_COLUMN; ++x) {
+			for (int y = 0; y < VIDEO_MAX_ROW; ++y) {
+				if (at(f, x, y) == ' ') {
+					continue;
+				}
+				for (int d = 0; d < 4; ++d) {
+					if (!within(x + dxy[d][0], y + dxy[d][1]) || at(f, x + dxy[d][0], y + dxy[d][1]) == ' ') {
+						continue;
+					}
+					join(label(x, y), label(x + dxy[d][0], y + dxy[d][1]));
+				}
+			}
+		}
+		for (int x = 0; x < VIDEO_MAX_COLUMN; ++x) {
+			for (int y = 0; y < VIDEO_MAX_ROW; ++y) {
+				char &ch = dsu[find(label(x, y)).first].second;
+				if (ch == '?') {
+					ch = dense_chars[rand() % 7];
+				}
+				set(f, x, y, ch);
+			}
+		}
+	}
+	
+	/* make boundary more variety. */
+	for (int f = 0; f < count; ++f) {
+		for (int x = 0; x < VIDEO_MAX_COLUMN; ++x) {
+			for (int y = 0; y < VIDEO_MAX_ROW; ++y) {
+				if (at(f, x, y) == ' ') {
+					continue;
+				}
+				int dir = -1, empty = 0;
+				for (int d = 0; d < 4; ++d) {
+					if (!within(x + dxy[d][0], y + dxy[d][1])) {
+						continue;
+					}
+					if (at(f, x + dxy[d][0], y + dxy[d][1]) == ' ') {
+						dir = d;
+						empty += 1;
+					}
+				}
+				if (dir != -1) {
+					char to_replace = dot_chars[dir];
+					if (empty <= 2 && 
+						at(f, x + dxy[(3 - dir) ^ 1][0], y + dxy[(3 - dir) ^ 1][1]) != ' ' &&
+						at(f, x + dxy[3 - dir][0], y + dxy[3 - dir][1]) != ' ') {
+							to_replace = line_chars[dir];
+					}
+					set(f, x, y, to_replace);
+				}
+			}
+		}
+	}
 }
 
 int video::progress(void) const {
--- kernel/ba/video.h
+++ kernel/ba/video.h
@@ -2,6 +2,8 @@
 #define __VIDEO_H__
 
 #include <stdint.h>
+#include <vector>
+#include <pair>
 
 namespace badapple {
 	
@@ -13,8 +15,16 @@ public:
 	bool has_next(void) const;
 	void free(void);
 private:
+	void artify(void);
+	char at(int, int, int) const;
+	void set(int, int, int, char);
+	
+	std::pair<int, char> find(int);
+	void join(int, int);
+private:
 	uint16_t *pool;
 	int count, cur_frame;
+	std::vector<std::pair<int, char>> dsu;
 };
 
 } /* badapple */
